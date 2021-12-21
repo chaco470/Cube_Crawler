6e8d66138b59200e030dd316babb46ba5792ddf3
chaco470 <marianolopez.com.26@gmail.com>
2021-12-21 14:43:26 -0300

    Cube Crawler commit inicial

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Sprite[] spriteArray;
+
+    private SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
